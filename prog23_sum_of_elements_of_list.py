50a82d87ee42445b9c8ee3e4df8509727b8cffe6
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:50:00 +0530

    Delete prog23_sum_of_elements_of_list.py

--- prog23_sum_of_elements_of_list.py
+++
@@ -1,6 +0,0 @@
-#Write program to the sum of elements of the list
-numbers = list(map(int,input('Enter the list elements:').split()))
-sum_list = 0
-for i in range(len(numbers)):
-    sum_list += numbers[i]
-print('Sum of list elements:',sum_list)
