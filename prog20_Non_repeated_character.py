9bda3b8e9d5ed01a28e80aaed4c94b87d77de216
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:50:37 +0530

    Delete prog20_Non_repeated_character.py

--- prog20_Non_repeated_character.py
+++
@@ -1,6 +0,0 @@
-#Write program to print the first non repeated character
-string = input('Enter a String:')
-for i in string:
-    if string.count(i) == 1:
-        print('First non repated character:',i)
-        break
