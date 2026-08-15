c22d5d6e4b668a7ce76b058807404fba853d17b1
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:51:22 +0530

    Delete prog16_count_occurrence_of_substrin.py

--- prog16_count_occurrence_of_substrin.py
+++
@@ -1,8 +0,0 @@
-#Write a program to count the occurrence of a substring
-string = input('Enter a string:')
-substring = input('Enter a substring:')
-count = 0
-for i in range(len(string) - len(substring) + 1):
-    if string[i:i + len(substring)] == substring:
-        count += 1
-print('Total number of substring',count)
