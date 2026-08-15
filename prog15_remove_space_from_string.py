1996f24242d1e94d19888d8ca11f55a69cbfd7fe
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:51:54 +0530

    Delete prog15_remove_space_from_string.py

--- prog15_remove_space_from_string.py
+++
@@ -1,5 +0,0 @@
-#Write a program to remove all spaces from the string
-word = input('Enter a String:')
-for i in word:
-    spaces_removed = word.replace(" ","")
-print('String after removing spaces',spaces_removed)
