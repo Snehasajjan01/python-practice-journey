0ebe44e78adcafe2b6b1fcf113d3a67b8a5c3d3c
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:50:48 +0530

    Delete prog19_check_if_strings_are_anagram.py

--- prog19_check_if_strings_are_anagram.py
+++
@@ -1,7 +0,0 @@
-#Write a program to check if strings are anagram
-string1 = input('Enter a string1:')
-string2 = input('Enter a string2:')
-if sorted(string1) == sorted(string2):
-    print('Two strings are anagram')
-else:
-    print('Given strings are not anagram')
