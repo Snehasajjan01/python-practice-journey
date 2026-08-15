13897fa3d57aeeb91263265f7f6d8de386a9d373
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:53:20 +0530

    Delete prog10sum_of_digits.py

--- prog10sum_of_digits.py
+++
@@ -1,9 +0,0 @@
-#Write a program to print the sum of digits of the given number
-n = int(input('Enter a number:'))
-sum_of_digits = 0
-while n != 0:
-    last_digit = n % 10
-    sum_of_digits = sum_of_digits + last_digit
-    n = n // 10
-    
-print('Sum of Digits =',sum_of_digits)
