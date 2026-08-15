1b418d6ea996b0262432478abb9240d2791aaa10
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:52:05 +0530

    Delete prog14_find_length_of_string.py

--- prog14_find_length_of_string.py
+++
@@ -1,6 +0,0 @@
-#Write a program to find the length of the string without using length function
-word = input('Enter a String:')
-count = 0
-for i in word:
-    count += 1
-print('Length of the String:',count)
