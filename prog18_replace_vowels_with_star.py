b1d32be169f7d0fd196e562b1e6c74e098c6e2f0
Sneha G Sajjan <94186460+Snehasajjan01@users.noreply.github.com>
2026-08-15 23:51:00 +0530

    Delete prog18_replace_vowels_with_star.py

--- prog18_replace_vowels_with_star.py
+++
@@ -1,8 +0,0 @@
-#Write program to replace vowels with *
-string = input('Enter a String:')
-replaced = string.replace('a','*')
-replaced = replaced.replace('e','*')
-replaced = replaced.replace('i','*')
-replaced = replaced.replace('o','*')
-replaced = replaced.replace('u','*')
-print('String after replacing vowels:',replaced) 
